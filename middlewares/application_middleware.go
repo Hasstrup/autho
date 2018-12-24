f33b7079815ef07153df91d364eeb13801aeb4d3
Hasstrup Ezekiel <hasstrup.ezekiel@gmail.com>
2018-12-24 19:38:46 +0100

    completes validating datatypes for schemas

--- middlewares/application_middleware.go
+++ middlewares/application_middleware.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 	"reflect"
 
@@ -23,7 +24,7 @@ func SanitizeApplicationRequest(next http.Handler) http.Handler {
 			return
 		}
 		go checkForEmptyValuesInBody(request, ch, &numberOfChecksDone)
-		go validateDatabaseStructure(request["database"], ch, &numberOfChecksDone)
+		go validateDatabaseStructure(request["database"], ch, &numberOfChecksDone, request["app_schema"])
 		for msg := range ch {
 			switch msg.(type) {
 			case string:
@@ -34,7 +35,6 @@ func SanitizeApplicationRequest(next http.Handler) http.Handler {
 				}
 			}
 		}
-
 		if len(errors) > 0 {
 			utils.RespondWithJSON(w, 400, map[string][]string{"errors": errors})
 			return
@@ -62,15 +62,61 @@ func checkForLength(t reflect.Type, entity interface{}) bool {
 	}
 }
 
-func validateDatabaseStructure(e interface{}, ch chan interface{}, counter *int) {
+func validateDatabaseStructure(e interface{}, ch chan interface{}, counter *int, schema interface{}) {
 	if _, ok := e.(string); !ok {
 		ch <- "Database must be a string"
 	} else {
 		if !utils.Contains(e, PermittedDatabaseTypes) {
 			ch <- "Currently we only support postgresql and mongodb"
+		} else {
+			// check that the schema is a map
+			if reflect.TypeOf(schema).Name() != "map" {
+				ch <- "app_schema must be an object containing the fields that are either required"
+			} else {
+				for key, value := range schema.(map[string]interface{}) {
+					// TODO: refactor this whole thing, so messy and long I could barely breathe
+					// reading this thing
+					if val, ok := value.(string); ok {
+						valid, err := isValidDataType(e.(string), val)
+						if err != nil {
+							ch <- err.Error()
+							break
+						}
+						if !valid {
+							ch <- key + " is not a supported datatype for the database provided"
+						}
+						continue
+					}
+					if val, ok := value.(map[string]interface{}); ok {
+						if _, present := val["type"]; !present {
+							ch <- "Please specify the datatype for " + key
+						} else {
+							// HMMMMM potential bug here
+							str, ok := val["type"].(string)
+							if valid, _ := isValidDataType(e.(string), str); ok && valid {
+								continue
+							}
+							ch <- "The type specified for " + key + " must either be 'string' or 'number'"
+						}
+
+					}
+				}
+			}
 		}
 	}
 	*counter++
 	ch <- true
+}
 
+func isValidDataType(database, value string) (bool, error) {
+	var permittedTypes map[string][]interface{}
+	permittedTypes["postgres"] = []interface{}{"string", "number"}
+	permittedTypes["mongodb"] = []interface{}{"string", "number"}
+	if _, ok := permittedTypes[database]; !ok {
+		return false, errors.New("The database provided isn't supported yet :)")
+	}
+	if !utils.Contains(value, permittedTypes[database]) {
+		return false, nil
+	}
+	return true, nil
 }
