bae6bc64af5087b1a731dc0d5c08ba4f0b1829be
Hasstrup Ezekiel <hasstrup.ezekiel@gmail.com>
2019-04-20 02:58:01 +0100

    tests (running but failing)

--- services/application_service_test.go
+++ services/application_service_test.go
@@ -2,13 +2,11 @@ package services
 
 import (
 	"context"
-	"fmt"
 	"testing"
 	"time"
 
 	"github.com/authenticate/models"
 	"github.com/mongodb/mongo-go-driver/mongo"
-	"github.com/mongodb/mongo-go-driver/mongo/options"
 )
 
 var body = map[string]interface{}{
@@ -19,45 +17,57 @@ var body = map[string]interface{}{
 	"app_key":  "key",
 	"database": "test_database",
 	"api_key":  "test_api_key",
+	"address":  "test address",
 }
 
 var model = models.NewApplicationModel(body)
-var service = ApplicationService{
-	Model: model,
-}
 
 func TestMain(t *testing.T) {
-	//setUp
-	SeedDatabase()
-	t.Run("find_one", testFindOne())
+	client := SeedDatabase()
+	// clear up db after test
+	defer func() {
+		if err := recover(); err != nil {
+			dropDatabase(client)
+		}
+	}()
+	t.Run("FindOneApplication", testFindOneApplication(client))
+	dropDatabase(client)
 }
 
-func testFindOne() func(t *testing.T) {
+func testFindOneApplication(client *mongo.Client) func(t *testing.T) {
 	return func(t *testing.T) {
-		fmt.Println("Okay")
+		query := map[string]string{
+			"name": "Test Application",
+		}
+		record := FindOneApplication(query, client)
+		expectedKey := "This is an application key"
+		if key, ok := record["app_key"].(string); !ok || key != expectedKey {
+			t.Errorf("expected app key to be This is an application key")
+		}
 	}
 }
 
-func SeedDatabase() {
+func SeedDatabase() *mongo.Client {
 	data := map[string]interface{}{
 		"name":     "Test Application",
 		"app_key":  "This is an application key",
 		"database": "here we go",
 	}
 
-	if client, err := initializeDatabase(); err != nil {
-		panic(err)
-	}
-	collection := client.Database("authenticate_test").Collection("application")
-	_, err := collection.insertMany(context.Background(), []interface{}{data})
+	client, err := initializeDatabase()
 	if err != nil {
 		panic(err)
 	}
+	collection := client.Database("authenticate_test").Collection("application")
+	if _, err := collection.InsertOne(context.Background(), data); err != nil {
+		panic(err)
+	}
+	return client
 }
 
 func initializeDatabase() (*mongo.Client, error) {
 	uri := "mongodb://localhost:27017"
-	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	client, err := mongo.NewClient(uri)
 	if err != nil {
 		return nil, err
 	}
@@ -68,4 +78,8 @@ func initializeDatabase() (*mongo.Client, error) {
 		return nil, err
 	}
 	return client, nil
+}
+
+func dropDatabase(client *mongo.Client) {
+	client.Database("authenticate_test").Drop(context.Background())
 }
